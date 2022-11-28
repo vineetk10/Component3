fc86721aa5c6b7e32491db9a3da7fa2d042ead83
vineetkarmiani <vineetkarmiani@gmail.com>
2022-11-28 15:30:48 -0800

    changes

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ app.use(cors({
 
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "http://44.211.76.239/:3000");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
